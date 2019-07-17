fad1bcd6ef346ea9e58c5e9bc8b4e5dc0ace61e4
Bounen057 <Bounen057@gmail.com>
2019-07-18 05:53:53 +0900

    [updata]コマンド処理

--- src/main/java/bounen057/adventureitems/AdventureItems.java
+++ src/main/java/bounen057/adventureitems/AdventureItems.java
@@ -1,11 +1,14 @@
 package bounen057.adventureitems;
 
+import bounen057.adventureitems.commands.GetCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdventureItems extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Bukkit.getPluginCommand("adventureitems").setExecutor(new GetCommand(this));
     }
 
     @Override
--- src/main/java/bounen057/adventureitems/commands/GetCommand.java
+++ src/main/java/bounen057/adventureitems/commands/GetCommand.java
@@ -1,4 +1,19 @@
 package bounen057.adventureitems.commands;
 
-public class GetCommand {
+import bounen057.adventureitems.AdventureItems;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GetCommand implements CommandExecutor {
+    private AdventureItems plugin;
+    public GetCommand(AdventureItems plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
 }
