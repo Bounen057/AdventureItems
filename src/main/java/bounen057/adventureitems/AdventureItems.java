0fffe89ef8f2ea46c441f806b13848354028ba9c
Bounen057 <Bounen057@Gmail.com>
2019-07-18 05:28:01 +0900

    追加

---
+++ src/main/java/bounen057/adventureitems/AdventureItems.java
@@ -0,0 +1,17 @@
+package bounen057.adventureitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdventureItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
